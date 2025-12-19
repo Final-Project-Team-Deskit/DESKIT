0846f2f558adfdd083a66c5566b2282514c278ea
dynii <dyniiyeyo@naver.com>
2025-12-19 11:52:42 +0900

    Project Setting

---
+++ src/test/java/com/deskit/deskit/DeskitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deskit.deskit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
