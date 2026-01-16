a8bbedb582d7189fa5938fe6ad88635a3ec797a0
dynii <dyniiyeyo@naver.com>
2026-01-16 22:27:56 +0900

    fix: Edit SecurityConfig.java (8)

--- src/main/java/com/deskit/deskit/common/SpaController.java
+++ src/main/java/com/deskit/deskit/common/SpaController.java
@@ -1,13 +1,23 @@
 package com.deskit.deskit.common;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = "/{path:^(?!api|ws|openvidu|oauth)(?!.*\\..*$).*$}/**")
-    public String forward() {
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    public String forward(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+
+        if (uri.startsWith("/api")
+                || uri.startsWith("/ws")
+                || uri.startsWith("/openvidu")
+                || uri.startsWith("/oauth")) {
+            return null; // forward하지 않음
+        }
+
         return "forward:/index.html";
     }
 }
