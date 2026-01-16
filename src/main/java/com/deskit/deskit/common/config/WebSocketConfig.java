0a1d09e9d64a1820ddc54ae44d597cf98d7aa08e
00parkyh <00parkyh@naver.com>
2026-01-16 11:41:53 +0900

    .withsockJS delte

--- src/main/java/com/deskit/deskit/common/config/WebSocketConfig.java
+++ src/main/java/com/deskit/deskit/common/config/WebSocketConfig.java
@@ -37,8 +37,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("https://ssg.deskit.o-r.kr", "http://localhost:3000", "*")
                 .addInterceptors(new WebSocketAuthHandshakeInterceptor(jwtUtil))
-                .setHandshakeHandler(new WebSocketAuthHandshakeHandler())
-                .withSockJS();
+                .setHandshakeHandler(new WebSocketAuthHandshakeHandler());
     }
 
     @Override
